fe8da66bec8b1447a673ad29103e3003ca1a3f59
novice108 <dshv937@meta.ua>
2020-12-12 14:51:03 +0200

    Update dwave_tsp.py

--- dwave_tsp.py
+++ dwave_tsp.py
@@ -1,29 +1,18 @@
 from collections import defaultdict
-
-from dwave.system.samplers import DWaveSampler
-from dwave.system.composites import EmbeddingComposite
 import networkx as nx
 
-import matplotlib
-matplotlib.use("agg")
-#from matplotlib import pyplot as plt
-
 # ------- Set up our graph -------
-
 # Create empty graph
 G = nx.complete_graph(4)
 G.add_weighted_edges_from({(0, 1, .1), (0, 2, .5), (0, 3, .1), (1, 2, .1), (1, 3, .5), (2, 3, .1)})
 
-lg = 1 # lagrange
-print("lg= ", lg)
-
-import dwave_networkx as dnx
+from dwave.system.samplers import DWaveSampler
+from dwave.system.composites import EmbeddingComposite
 import dimod
-#ans = dnx.traveling_salesperson (G, sampler=dimod.ExactSolver(), lagrange=lg, weight='weight', start=0)
+
 ans = dnx.traveling_salesperson (G, sampler=dimod.ExactSolver(), start=0)
 print("ExactSolver [0 1 2 3]") 
 print(ans)
-
 
 # ------- Set up our QUBO dictionary -------
 #
@@ -39,9 +28,8 @@ for i, j in G.edges:
 # ------- Run our QUBO on the QPU -------
 # Set up QPU parameters
 chainstrength = 1.0
-numruns = 100
+numruns = 10
 
-#Q = dnx.traveling_salesperson_qubo(G, lagrange=lg, weight='weight')
 Q = dnx.traveling_salesperson_qubo(G)
 
 # Run the QUBO on the solver from your config file
@@ -51,7 +39,6 @@ energies = iter(response.data())
 print("response")
 print(response)
 
-#d_answer = dnx.traveling_salesperson(G, sampler, lagrange=lg, weight='weight', start=0)
 d_answer = dnx.traveling_salesperson(G, sampler, start=0)
 print("QuantumSolver")
 print(d_answer)
